5393b8a3b492e676f2a9258456df85f65c5c7d10
ArduCrow <arducrow@proton.me>
2024-06-29 23:25:06 +0100

    connected on first heartbeat logic and mutex to prevent race conditions on state update

--- internal/vehicle/vehicle.go
+++ internal/vehicle/vehicle.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"gomavlink/internal/mavlink"
 	"gomavlink/internal/reader"
+	"sync"
 )
 
 type Airframe int
@@ -39,12 +40,12 @@ type Position struct {
 }
 
 type Vehicle struct {
-	// TODO - Add fields to represent the vehicle's state
+	connected  bool
 	connection *reader.MavlinkReader
 	Airframe   Airframe
 	Battery    Battery
 	Position   Position
-	// TODO - make the reader a field of the vehicle, make the reader update the vehicle's state as updated messages are received
+	lock       sync.Mutex
 }
 
 func NewVehicle(port string, baud int, network bool) (*Vehicle, error) {
@@ -56,7 +57,6 @@ func NewVehicle(port string, baud int, network bool) (*Vehicle, error) {
 		return nil, err
 	}
 	defer r.Close()
-
 	return &Vehicle{connection: r}, nil
 }
 
@@ -77,32 +77,50 @@ func (v *Vehicle) Travel(lat float64, lon float64, alt int) {
 }
 
 func (v *Vehicle) updateStates(msg mavlink.DecodedMessage) {
+	// prevent race conditions/multiple subprocecces from updating the states
+	v.lock.Lock()
+	defer v.lock.Unlock()
 
-	switch msg.GetMessageID() {
-	case 0:
-		// Heartbeat
-		rawAirframe := msg.MessageData()["Type"].(float64)
-		intAirframe := int(rawAirframe)
-		v.Airframe = Airframe(intAirframe)
-		fmt.Println("Airframe: ", v.Airframe.String())
-	case 33:
-		// GlobalPositionInt
-		v.connection.CurrentStates.GlobalPositionIntState = msg.MessageData()
-	case 74:
-		// VFR_HUD
-		v.connection.CurrentStates.VFRHUDState = msg.MessageData()
-	default:
-		fmt.Println("Unknown message ID: ", msg.GetMessageID())
+	if msg.GetMessageID() == 0 && !v.connected {
+		v.processInitialHeartbeat(msg)
+	} else if v.connected {
+		// Process other messages only if connected is true
+		switch msg.GetMessageID() {
+		case 0:
+			// Heartbeat
+		case 33:
+			// GlobalPositionInt
+			v.connection.CurrentStates.GlobalPositionIntState = msg.MessageData()
+		case 74:
+			// VFR_HUD
+			v.connection.CurrentStates.VFRHUDState = msg.MessageData()
+		default:
+			fmt.Println("Unknown message ID: ", msg.GetMessageID())
+		}
 	}
 
-	// v.updateBatteryState(latestVFRHUDMessageData["BatteryVoltage"].(float64), latestVFRHUDMessageData["BatteryCurrent"].(float64))
-	v.updatePosition(
-		v.connection.CurrentStates.GlobalPositionIntState["Lat"].(float64),
-		v.connection.CurrentStates.GlobalPositionIntState["Lon"].(float64),
-		v.connection.CurrentStates.GlobalPositionIntState["RelativeAlt"].(float64),
-		v.connection.CurrentStates.GlobalPositionIntState["Alt"].(float64),
-		v.connection.CurrentStates.GlobalPositionIntState["Hdg"].(float64),
-	)
+	if v.connected && v.connection.CurrentStates.GlobalPositionIntState != nil {
+		v.updatePosition()
+	}
+}
+
+func (v *Vehicle) processInitialHeartbeat(msg mavlink.DecodedMessage) {
+	rawAirframe := msg.MessageData()["Type"].(float64)
+	intAirframe := int(rawAirframe)
+	v.Airframe = Airframe(intAirframe)
+	fmt.Println("Airframe: ", v.Airframe.String())
+	v.connected = true
+}
+
+func (v *Vehicle) updatePosition() {
+	v.Position = Position{
+		Latitude:         v.connection.CurrentStates.GlobalPositionIntState["Lat"].(float64),
+		Longitude:        v.connection.CurrentStates.GlobalPositionIntState["Lon"].(float64),
+		AltitudeRelative: v.connection.CurrentStates.GlobalPositionIntState["RelativeAlt"].(float64),
+		AltitudeAMSL:     v.connection.CurrentStates.GlobalPositionIntState["Alt"].(float64),
+		Heading:          v.connection.CurrentStates.GlobalPositionIntState["Hdg"].(float64),
+	}
+
 	fmt.Println("Position state update: ", v.Position)
 }
 
@@ -112,13 +130,3 @@ func (v *Vehicle) updateStates(msg mavlink.DecodedMessage) {
 // 		Current: current,
 // 	}
 // }
-
-func (v *Vehicle) updatePosition(lat float64, lon float64, altRel float64, altAMSL float64, heading float64) {
-	v.Position = Position{
-		Latitude:         lat,
-		Longitude:        lon,
-		AltitudeRelative: altRel,
-		AltitudeAMSL:     altAMSL,
-		Heading:          heading,
-	}
-}
